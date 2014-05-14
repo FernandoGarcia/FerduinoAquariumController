ff72001d015781785c48746dc956fb87fe5d1068
Fernando Garcia <fefegarcia_1@hotmail.com>
2014-05-14 11:22:56 -0300

    Atualização importante.

--- Bibliotecas/UIPEthernet/utility/uipethernet-conf.h
+++ Bibliotecas/UIPEthernet/utility/uipethernet-conf.h
@@ -9,10 +9,13 @@
 #define UIP_CONF_UDP             1
 #define UIP_CONF_BROADCAST       1
 #define UIP_CONF_UDP_CONNS       4
-#define UIP_UDP_NUMPACKETS       5
 
 /* number of attempts on write before returning number of bytes sent so far
  * set to -1 to block until connection is closed by timeout */
 #define UIP_ATTEMPTS_ON_WRITE    -1
+
+/* timeout after which UIPClient::connect gives up. The timeout is specified in seconds.
+ * if set to a number <= 0 connect will timeout when uIP does (which might be longer than you expect...) */
+#define UIP_CONNECT_TIMEOUT      -1
 
 #endif
