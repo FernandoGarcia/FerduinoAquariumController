76171d30543aba01650902be73bb03fcf00695f6
Fernando Garcia <fefegarcia_1@hotmail.com>
2014-09-20 08:53:38 -0300

    Ajuste importante.

--- Feduino_with_webcontrol_beta/SPI.ino
+++ Feduino_with_webcontrol_beta/SPI.ino
@@ -6,18 +6,21 @@ void selecionar_SPI(int dispositivo)
     digitalWrite(ChipSelect_SD, LOW);
     digitalWrite(SelectSlave_ETH, HIGH);
     digitalWrite(ChipSelect_RFM, HIGH);
+    delay(10);
     break; 
 
   case 1: // Utilizar ethercard
     digitalWrite(SelectSlave_ETH, LOW);
     digitalWrite(ChipSelect_SD, HIGH);
     digitalWrite(ChipSelect_RFM, HIGH);
+    delay(10);
     break; 
 
   case 2: // Utilizar RFM12B
     digitalWrite(ChipSelect_RFM, LOW);
     digitalWrite(SelectSlave_ETH, HIGH);
     digitalWrite(ChipSelect_SD, HIGH);
+    delay(10);
     break; 
   }
   return;
